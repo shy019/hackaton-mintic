7a4b8c0fc605b2034639739874bd68e5635a732f
DESKTOP-DRHRRQM\cristian <cristianpg123@hotmail.com>
2021-10-09 17:33:06 -0500

    Borrado de backend

--- hackaton-mintic-backend/src/main/java/co/com/mintic/hackaton/payload/request/LogOutRequest.java
+++
@@ -1,9 +0,0 @@
-package co.com.mintic.hackaton.payload.request;
-
-public class LogOutRequest {
-  private Long userId;
-
-  public Long getUserId() {
-    return this.userId;
-  }
-}
--- hackaton-mintic-backend/src/main/java/co/com/mintic/hackaton/util/Util.java
+++
@@ -1,5 +0,0 @@
-package co.com.mintic.hackaton.util;
-
-public class Util {
-	public static final String URL_BASE = "http://localhost:8090";
-}
